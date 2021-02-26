25a319aee33a3b98b345eb6d81fd26f3a609ec2a
Keriv-cyber <77460846+Keriv-cyber@users.noreply.github.com>
2021-02-26 13:11:07 -0500

    Add files via upload

---
+++ Deliverable6_Website.css
@@ -0,0 +1,102 @@
+<!DOCTYPE html>
+<html lang="en">
+	<head>
+		<meta charset="UTF-8">
+		<title align="center"> Class Project - Deliverable 6 - Website </title>
+	</head>
+	
+	<body style="background-color:lightgrey"
+	
+	<h1 style="font-color:navy" align="center"> Class Project - Deliverable 6 - Website </h1>
+	
+	<p style="page-break-before:ALways">
+	<img src="Keri Vollenweider Professional Headshot.PNG" width="225px" height="195px" />
+	
+	<h1 align="center"> Keri Lee Vollenweider </h1>
+	
+	<h3 align="center"> KeriV@usf.edu  |  (813) 326-9904 </h3>
+	
+	<p> As a Senior in Business Analytics and Information Systems at the University of South Florida, I look to add value with my abilities to encourage innovative ideas through my intellectual curiosity and contribute bold new ideas amongst future coworkers. </p>
+	
+	<hr>
+	
+	<h3 align="left"> <u> Education </u> </h3>
+	<p style="page-break-before:always">
+	<p align="left"> Bachlors  |  May 2021 </p>
+	<p style="page-break-before:always">
+	<p align="left"> <i> University Of South Florida </i> </p>
+	<p style="page-break-before:always">
+	<p align="left"> Business Analytics and Information Systems, Major </p>
+	<p style="page-break-before:always">
+	<p align="left">  Cumulative GPA of 3.76/4.0  </p>
+	<p style="page-break-before:always">
+	<p align="left"> Dean's List Spring 2018 </p>
+	<p style="page-break-before:always">
+	<p align="left">  Certificate of Achievement in Principals of Financial Accounting 2021, Spring 2019 </p>
+	<p style="page-break-before:always">
+	<p align="left"> Dean's List Spring 2019  </p>
+	
+	
+	<h3 align="left"> <u> Skills & Abilities </u> </h3>
+	<p style="page-break-before:always">
+	<ul>
+	<li align="left">  Advanced Certification in Microsoft Office; Word, Excel, PowerPoint, and Access </li>
+	<li align="left"> Immense ability to work effectively in a group setting due to four years of working in hospitality </li>
+	<li align="left"> Proficient at multi-tasking due to the fast-paced atmosphere of my working environment </li>
+	<li align="left"> Skilled in SQL, C#, JavaScript, Visio, C++, HTML </li>
+	</ul>
+	
+	
+	<h3 align="left"> <u> Experience </u> </h3>
+	<ol>
+		<li align="left"> Target Seasonal Employee, November 2020 - January 2021</li>
+		<li align="left"> East Lake Woodlands Country Club, June 2017 - March 2019</li>
+		<li align="left"> Clearwater Yacht Club, July 2015 - December 2017</li>
+		<li align="left"> Complete Auctions, July 2015 - December 2018</li>
+	</ol>
+	
+	<hr>
+	
+	<h3 align="left"> <u> Academic History </u> </h3>
+	<p style="page-break-before:always">
+	<p align="left"> Courses that align with Major: </p>
+	
+	<p style="page-break-before:always">
+	<ul>
+	<li align="left"> ISM3011: Information Systems in Organizations </li>
+	<li align="left"> ISM3113: Systems Analysis & Design  </li>
+	<li align="left"> ISM3232: Business Application Development </li>
+	<li align="left"> ISM4041: Global Cyber Ethics </li>
+	<li align="left"> ISM4300: Project Management  </li>
+	<li align="left"> ISM4323: Information Security/IT Risk Management </li>
+	<li align="left"> ISM4212: Database Desgin/Administration </li>
+	<li align="left"> ISM4220: Business Data Communications </li>
+	</ul>
+	
+	<p style="page-break-before:always">
+	<p align="left"> USF Class Projects: </p>
+	
+	<p style="page-break-before:ALways">
+	<img src="Class Project 1.PNG" width="305px" height="200px" align="left"/>
+	
+	<p style="page-break-before:ALways">
+	<img src="Class Project 2.PNG" width="305px" height="200px" align="center"/>
+	
+	<hr>
+	
+	<h3 align="left"> <u> About Me </u> </h3>
+	<p style="page-break-before:always">
+	<p align="left"> <i> Why Analytics? </i> </p>
+	
+	<p style="page-break-before:always">
+	<p align="left"> I chose Business analytics and information systems as a major because it provides me with the necessary skills for the business world, knowledge in data analytics, and developmental tools for information systems. This major will lead me to positions not only in business organizations, but also non-business organizations. Analytics will allow me to compute and analyze data and statistics over time, where information systems will allow me to obtain necessary skills to collect, process, and store information that supports decision making. </p>
+	
+	
+	<p style="page-break-before:always">
+	<a href="https://www.linkedin.com/in/keri-v-5b7686106/"> Connect with me on Linkedin!</a>
+	
+	
+	</body>
+	
+</html>
+		
